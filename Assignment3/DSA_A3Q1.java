6a1f83d358bb8859069d5ab7fe474e6e833990fa
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2021-07-30 19:30:20 +0530

    Add files via upload

---
+++ Assignment3/DSA_A3Q1.java
@@ -0,0 +1,65 @@
+package com.company;
+import java.util.Scanner;
+
+public class DSA_A3Q1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int m=scan.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int n=scan.nextInt();
+        int [][] arr = new int [m][n];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0; i<m;i++)
+        {
+            for(int j=0; j<n;j++)
+            {
+                arr[i][j]=scan.nextInt();
+            }
+        }
+
+//        int [][] arr = {{11, 12, 13, 14}, {21, 22, 23, 24}, {31, 32, 33, 34}, {41, 42, 43, 44}};
+
+        int tle = arr.length * arr[0].length;
+
+        int sr=0, sc=0;
+        int er = arr.length - 1, ec = arr[0].length - 1;
+        int dir = -1;
+
+        while(tle != 0){
+            dir = (dir + 1) % 4;
+
+            if(dir==0){
+                for(int i=sr; i<=er; i++){
+                    System.out.print(arr[i][sc] + " ");
+                    tle--;
+                }
+                sc++;
+            }
+            else if(dir==1){
+                for(int i=er; i>=sr; i--){
+                    System.out.print(arr[i][sc] + " ");
+                    tle--;
+                }
+                sc++;
+            }
+            else if(dir==2){
+                for(int i=sr; i<=er; i++){
+                    System.out.print(arr[i][sc] + " ");
+                    tle--;
+                }
+                sc++;
+            }
+            else{
+                for(int i=er; i>=sr; i--){
+                    System.out.print(arr[i][sc] + " ");
+                    tle--;
+                }
+                sc++;
+            }
+        }
+
+
+    }
+}
+
