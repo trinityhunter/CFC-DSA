30b84321c9998d14246264e986773dfef4033dca
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2021-08-15 16:54:23 +0530

    Add files via upload

---
+++ Assignment3/DSA_A3Q14c.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DSA_A3Q14c {
+    public static void main(String[] args) {
+        int [] nums = {0,1,2,3,4};
+        int [] index = {0,1,2,2,1};
+
+        ArrayList<Integer> arr = new ArrayList<Integer> (nums.length);
+        int [] ans = new int [nums.length];
+
+        for(int i=0; i<nums.length; i++){
+            arr.add(index[i], nums[i]);
+        }
+        for(int i=0; i<nums.length; i++){
+            ans[i] = arr.get(i);
+        }
+        System.out.println(Arrays.toString(ans));
+    }
+}
